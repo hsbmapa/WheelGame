f20a66ccb3cbcd66913ee3c494dee9a7663491ec
splishie <49469036+splishie@users.noreply.github.com>
2019-04-11 12:10:31 +1000

    Update GameEngineCallbackImpl.java

--- src/view/GameEngineCallbackImpl.java
+++ src/view/GameEngineCallbackImpl.java
@@ -13,10 +13,8 @@ import java.util.logging.Logger;
  * @author Caspar Ryan
  * @see view.interfaces.GameEngineCallback
  */
-@SuppressWarnings("Duplicates")
 public class GameEngineCallbackImpl implements GameEngineCallback {
     private static final Logger logger = Logger.getLogger(GameEngineCallback.class.getName());
-
 
     public GameEngineCallbackImpl() {
         // FINE shows wheel spinning output, INFO only shows result
@@ -32,11 +30,10 @@ public class GameEngineCallbackImpl implements GameEngineCallback {
         //logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
         int pos = slot.getPosition();
         int num = slot.getNumber();
-        model.enumeration.Color col = slot.getColor();
         //making color proper case
-        String colour = col.name();
-        colour = colour.substring(0, 1).toUpperCase() + colour.substring(1).toLowerCase();
-        String result = String.format("Position: %s, Color: %s, Number: %s", pos, colour, num);
+        String col = slot.getColor().name();
+        col = col.substring(0, 1).toUpperCase() + col.substring(1).toLowerCase();
+        String result = String.format("Position: %s, Color: %s, Number: %s", pos, col, num);
         logger.log(Level.FINE, result);
     }
 
@@ -47,11 +44,10 @@ public class GameEngineCallbackImpl implements GameEngineCallback {
         //logger.log(Level.FINE, "Result data to log .. String.format() is good here!");
         int pos = result.getPosition();
         int num = result.getNumber();
-        model.enumeration.Color col = result.getColor();
         //making color title case
-        String colour = col.name();
-        colour = colour.substring(0, 1).toUpperCase() + colour.substring(1).toLowerCase();
-        logger.log(Level.INFO, String.format("RESULT = Position: %s, Color: %s, Number: %s\n", pos, colour, num));
+        String col = result.getColor().name();
+        col = col.substring(0, 1).toUpperCase() + col.substring(1).toLowerCase();
+        logger.log(Level.INFO, String.format("RESULT = Position: %s, Color: %s, Number: %s\n", pos, col, num));
         engine.calculateResult(result);
         logger.log(Level.INFO, "FINAL PLAYER POINT BALANCES");
         logger.log(Level.INFO, engine.getAllPlayers().toString().replace("[", "").replace("]",
